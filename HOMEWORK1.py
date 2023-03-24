5efe0dc8449dfd2c1084d1451b7fd5d734c8a597
Geetha Suresh <geethaskbaaa7255@gmail.com>
2023-03-24 18:22:27 -0400

    My First Checkin

---
+++ HOMEWORK1.py
@@ -0,0 +1,8 @@
+print('GeethaSuresh')
+a='GeethaSuresh'
+print(a[-1]+a[-2]+a[-3]+a[-4]+a[-5]+a[-6]+a[-7]+a[-8]+a[-9]+a[-10]+a[-11]+a[-12])
+b=''
+for i in range(1,13):
+    b=b+a[-i]
+    # print(b)
+print(b)
